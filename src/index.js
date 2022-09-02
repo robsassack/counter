fe8c943dd17e99454e506a0f8e1ad324f57396db
Rob Sassack <rsassack25@gmail.com>
2022-09-02 17:53:19 -0400

    Added buttons for increasing and decreasing in CSS

--- src/index.js
+++ src/index.js
@@ -5,5 +5,19 @@ const content = document.querySelector('.content');
 const counter = document.createElement('div');
 counter.classList.add('counter');
 counter.classList.add('one');
-counter.textContent = 0;
+
+const counterValue = document.createElement('span');
+counterValue.textContent = 0;
+counter.appendChild(counterValue);
+
+const decrease = document.createElement('div');
+decrease.classList.add('decrease');
+decrease.textContent = '−';
+counter.appendChild(decrease);
+
+const increase = document.createElement('div');
+increase.classList.add('increase');
+increase.textContent = '+';
+counter.appendChild(increase);
+
 content.appendChild(counter);
